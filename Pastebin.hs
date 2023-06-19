36342569dab2d3e1bc06f2c91c7c5af17d84bfc5
mniip <mniip@mniip.com>
2023-06-19 19:42:58 +0200

    Use tomsmeding's pastebin

--- Pastebin.hs
+++ Pastebin.hs
@@ -4,8 +4,11 @@ module Pastebin where
 
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
-import Network.Socket
-import Network.Socket.ByteString as NBS
+import qualified Data.Text.Encoding as T
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS
+import Network.HTTP.Types
+import Web.FormUrlEncoded
 
 pastebin_addr = "tcp.st"
 pastebin_port = 7777
@@ -14,28 +17,19 @@ pasteUrlLength :: Int
 pasteUrlLength = 25
 
 paste :: ByteString -> IO ByteString
-paste text = do addrs <- getAddrInfo Nothing (Just pastebin_addr) (Just $ show pastebin_port)
-                case addrs of
-                  [] -> error "Could not resolve hostname"
-                  addr:_ -> do
-                    sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
-                    connect sock (addrAddress addr)
-                    sendAll sock text
-                    recvUrl sock ""
-  where
-    sendAll sock bs = do i <- NBS.send sock bs
-                         if | i == BS.length bs -> pure ()
-                            | i > 0 -> sendAll sock (BS.drop i bs)
-                            | otherwise -> pure ()
-    
-    recvUrl sock buf | BS.isInfixOf "\n" buf
-                     , (line, rest) <- BS.breakSubstring "\n" buf
-                     , (word, param) <- BS.breakSubstring " " line
-                     = if | word == "URL" -> pure $ BS.tail param
-                          | word == "ERROR" -> error $ show param
-                          | otherwise -> recvUrl sock (BS.tail rest)
-                     | otherwise
-                     = do bs <- NBS.recv sock 4096
-                          if BS.null bs
-                          then error "No URL received"
-                          else recvUrl sock (BS.append buf bs)
+paste text = do mgr <- newManager tlsManagerSettings
+                let req = "POST https://paste.tomsmeding.com/paste"
+                      { requestHeaders = [("Content-Type", "application/x-www-form-urlencoded")]
+                      , requestBody = RequestBodyLBS $ urlEncodeForm $ toForm
+                          [ ("name1" :: String, "")
+                          , ("code1", T.decodeUtf8 text)
+                          , ("expire", "day")
+                          ]
+                      , redirectCount = 0
+                      }
+                resp <- httpNoBody req mgr
+                if responseStatus resp /= seeOther303
+                then error $ "Unexpected response status: " ++ show (responseStatus resp)
+                else case lookup "Location" $ responseHeaders resp of
+                  Nothing -> error "No location header"
+                  Just loc -> pure $ "https://paste.tomsmeding.com" <> loc
